c913371af5feab9b40e13d65ef1f8bfe057666f0
ify <oge_umerah25@milton.edu>
2023-04-24 14:06:58 -0400

    2d array

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,5 +1,18 @@
-public class Main {
+public class MyFarm {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyFarm fruit = new  MyFarm();
+    }
+    public MyFarm(){
+        System.out.println("hello from my farm");
+        Plot P1 = new Plot();
+        P1.printPlot();
+
+        Plot[][] grid = new Plot [10][10];
+        for (int x = 0; x < grid.length; x++) {
+            for (int h = 0; h < grid[x].length; h++) {
+            grid[x][h] = new Plot();
+            grid[x][h].printPlot();
+            }
+        }
     }
 }
