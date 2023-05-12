c7fa6b47f599a4344d206326aceda3ea9b57454b
ify <oge_umerah25@milton.edu>
2023-05-12 13:07:18 -0400

    array list

---
+++ src/MyFarmArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class MyFarmArrayList {
+   public ArrayList<Plot> row = new ArrayList<Plot>();
+    public Plot[] arr = new Plot[10];
+    public MyFarmArrayList(){
+        System.out.println("my farm using array list");
+
+        arr[0] = new Plot();
+
+        for (int p = 0; p<10; p++){
+            System.out.println(row.size());
+            row.add(new Plot());
+            row.get(p).printPlot();
+        }
+        row.add(new Plot());
+        System.out.println(row.size());
+    }
+
+}
